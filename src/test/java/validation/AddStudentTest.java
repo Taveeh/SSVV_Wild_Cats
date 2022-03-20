80b0d47cfff31fd57343c59c2696e4f6b9ac83f0
Ana-Maria Demian <anamariadem7@gmail.com>
2022-03-20 15:56:46 +0200

    JUnit works now

---
+++ src/test/java/validation/AddStudentTest.java
@@ -0,0 +1,12 @@
+package validation;
+
+import org.junit.jupiter.api.Test;
+import repository.StudentXMLRepository;
+
+public class AddStudentTest {
+    private StudentXMLRepository studentXMLRepository;
+
+    @Test
+    void validate() {
+    }
+}
