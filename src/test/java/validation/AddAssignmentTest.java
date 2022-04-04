2b96e9d8c7c4a58a023025f0aa2c1b653444a9c0
Ana-Maria Demian <anamariadem7@gmail.com>
2022-04-04 22:33:42 +0300

    added tests for addAssignmeny

--- src/test/java/validation/AddAssignmentTest.java
+++ src/test/java/validation/AddAssignmentTest.java
@@ -10,18 +10,74 @@ public class AddAssignmentTest {
     private TemaRepository temaRepository = new TemaRepository(new TemaValidator());
 
     @Test
-    void testValidAssignment_ShouldWork() {
-        Tema tema = new Tema("1", "test", 12, 4);
+    void tc1_TestValidAssignment_ShouldWork() {
+        Tema tema = new Tema("1", "test", 12, 10);
 
         assertDoesNotThrow(() ->  temaRepository.save(tema));
         assertEquals(1, temaRepository.findAll().spliterator().estimateSize());
     }
 
     @Test
-    void testInvalidAssignment_DuplicateId_ShouldThrowError() {
+    void tc2_TestInvalidAssignment_NullId_ShouldThrowError() {
+        Tema tema = new Tema(null, "test", 12, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc3_TestInvalidAssignment_NullDescription_ShouldThrowError() {
+        Tema tema = new Tema("1", null, 12, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc4_TestInvalidAssignment_StartlineGreaterThanDeadline_ShouldThrowError() {
+        Tema tema = new Tema("1", "test", 10, 12);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc5_TestInvalidAssignment_InvalidStartline_ShouldThrowError() {
+        Tema tema = new Tema("1", "test", 10, -1);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc6_testInvalidAssignment_DuplicateId_ShouldThrowError() {
         Tema tema = new Tema("1", "test", 12, 4);
 
         assertDoesNotThrow(() ->  temaRepository.save(tema));
         assertThrows(ValidationException.class, () -> temaRepository.save(tema));
+    }
+
+    @Test
+    void tc7_TestInvalidAssignment_EmptyId_ShouldThrowError() {
+        Tema tema = new Tema("", "test", 12, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc8_TestInvalidAssignment_EmptyDescription_ShouldThrowError() {
+        Tema tema = new Tema("1", "", 12, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc9_TestInvalidAssignment_InvalidDeadline_ShouldThrowError() {
+        Tema tema = new Tema("1", "test", -1, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
+    }
+
+    @Test
+    void tc10_TestInvalidAssignment_InvalidDeadline_ShouldThrowError() {
+        Tema tema = new Tema("1", "test", 15, 10);
+
+        assertThrows(ValidationException.class, () ->  temaRepository.save(tema));
     }
 }
